a399850d55efd2680c4a5a1aeb2df391337f07b2
eeminsso <alstn9623@vitcon.co.kr>
2021-03-26 15:28:44 +0900

    first commit

---
+++ src/secondThird.java
@@ -0,0 +1,15 @@
+
+public class secondThird {
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=9; i++)
+		{
+			System.out.println("2*"+(i)+"="+(2*i));
+		}
+		System.out.println("");
+		for(int i=1; i<=9; i++)
+		{
+			System.out.println("3*"+(i)+"="+(3*i));
+		}
+	}
+}
