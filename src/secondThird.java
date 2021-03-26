c1d4d74d42d60c1f900dc8f935f4c0777298a1b8
eeminsso <alstn9623@vitcon.co.kr>
2021-03-26 15:31:12 +0900

    2,3단 print 추가

--- src/secondThird.java
+++ src/secondThird.java
@@ -2,11 +2,13 @@
 public class secondThird {
 	public static void main(String[] args)
 	{
+		System.out.println("2단");
 		for(int i=1; i<=9; i++)
 		{
 			System.out.println("2*"+(i)+"="+(2*i));
 		}
 		System.out.println("");
+		System.out.println("3단");
 		for(int i=1; i<=9; i++)
 		{
 			System.out.println("3*"+(i)+"="+(3*i));
